025d6c3dc904ad73fd86791537c3c4d433dc2882
Albert <albert.xalimow@gmail.com>
2022-09-11 19:25:03 +0500

    feat bootstrap

---
+++ components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link to="/" exact active-class="active" class="nav-link">
+            Home
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/about" active-class="active" class="nav-link">
+            about
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>ABoutPage</h1>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia alias
+      ipsam blanditiis quisquam repellendus a. Voluptatibus fuga minima culpa
+      iste commodi corrupti nemo enim dignissimos soluta. Nulla at amet sint.
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
